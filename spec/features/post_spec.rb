74d23fa35909b1bda430ff9bc4ebd3c3c0d365cd
Tami <catquarks@gmail.com>
2016-09-15 16:21:11 -0400

    pass all but last test last test seemed irrelevant to the lesson

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -1,8 +1,9 @@
 require 'rails_helper'
 
 describe 'navigate' do
-  before do
-    @post = Post.create(title: "My Post", description: "My post desc")
+  before do 
+    author = Author.create(name: "monkey", hometown: "moon")
+    @post = Post.create(title: "My Post", description: "My post desc", author_id: 1)
   end
 
   it 'shows the title on the show page in a h1 tag' do
@@ -33,8 +34,9 @@ describe 'form' do
     expect(page).to have_content("My post title")
   end
 
-  it 'shows a new form that submits content and redirects and prints out params' do
-    @post = Post.create(title: "My Post", description: "My post desc")
+  it 'shows a new form that submits content and redirects and prints out params' do 
+    author = Author.create(name: "monkey", hometown: "moon")
+    @post = Post.create(title: "My Post", description: "My post desc", author_id: 1)
 
     visit edit_post_path(@post)
 
